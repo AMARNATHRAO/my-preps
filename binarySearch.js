280ca457c5dde08cd65e3300091167a75ddc0140
Amarnathrao Sulake <amarnath.sulake@leoforce.com>
2021-11-11 14:08:51 +0530

    :sparkle: Binary search

---
+++ binarySearch.js
@@ -0,0 +1,18 @@
+function binary_Search(items, value) {
+    let firstIndex = 0,
+        lastIndex = items.length - 1,
+        middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+
+    while (items[middleIndex] !== value && firstIndex < lastIndex) {
+        if (value < items[middleIndex]) {
+            lastIndex = middleIndex - 1;
+        } else if (value > items[middleIndex]) {
+            firstIndex = middleIndex + 1;
+        }
+        middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+    }
+    return items[middleIndex] != value ? -1 : middleIndex;
+}
+
+console.log(binary_Search([1, 2, 3, 4, 5], 5));
+console.log(binary_Search([1, 2, 3, 4, 5], 10));
